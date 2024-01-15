c34e3450c9d89444c444beee86e067c552fe5393
jeongyong <cin03023@gmail.com>
2024-01-15 21:35:40 +0900

    2) v-for 반복문

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,18 @@ import { ref } from 'vue';
 
 const price1 = ref(60)
 const price2 = ref(70)
+const menus = ['Home', 'Shop', 'About']
 const products = ['역삼동원룸', '천호동원룸', '마포구원룸']
 </script>
 
 <template>
-  <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-  <div>
-    <h4>{{ products[0] }}</h4>
+  <div class="menu">
+    <a v-for="menu in menus" :key="menu">{{ menu }}</a>
+  </div>
+
+  <div v-for="product in products" :key="product">
+    <h4>{{ product }}</h4>
     <p>50 만원</p>
-  </div>
-  <div>
-    <h4>{{ products[1] }}</h4>
-    <p>70 만원</p>
-  </div>
-  <div>
-    <h4>{{ products[2] }}</h4>
-    <p>90 만원</p>
   </div>
 </template>
 
@@ -29,5 +25,16 @@ const products = ['역삼동원룸', '천호동원룸', '마포구원룸']
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
